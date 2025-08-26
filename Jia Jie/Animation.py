4a55c98248bad7589673ef2fccd1036ee5abee92
Basil1361 <trainerbasil69@gmail.com>
2025-08-26 22:15:00 +0800

    Update

--- Jia Jie/Animation.py
+++ Jia Jie/Animation.py
@@ -14,9 +14,14 @@ ground_surface = pg.image.load("D:\Pygame\Jia Jie\InputFiles\graphics\ground.png
 # this is how you import images. Convert alpha makes the pictures easier to handle in the future when editing
 text_surface = font.render("Hello", True, "Black")
 # font.render(<text>, <anti-aliasing>, <color>)
+
 snail_surface = pg.image.load("D:\Pygame\Jia Jie\InputFiles\graphics\snail\snail1.png").convert_alpha()
-snail_x_pos = 600
+snail_rect = snail_surface.get_rect(bottomright = (600, 300))
+
 player_surface = pg.image.load("D:\Pygame\Jia Jie\InputFiles\graphics\player\player_walk_1.png").convert_alpha()
+player_rect = player_surface.get_rect(bottomright = (80, 300))
+
+# rect_surface = pg.Rect(left,top,width,height)
 
 while True:
     for event in pg.event.get():
@@ -26,11 +31,13 @@ while True:
     # screen.blit(obj, (coordinates))
     screen.blit(sky_surface,(0, 0))
     screen.blit(ground_surface,(0, 300))
-    screen.blit(text_surface,(350, 50))
-    snail_x_pos -= 2
-    screen.blit(snail_surface, (snail_x_pos,250))
-    if snail_x_pos < -100: 
-        snail_x_pos = 800
-    screen.blit(player_surface, (80, 200))
+    screen.blit(text_surface,(300, 50))
+    snail_rect.x -= 4
+    if snail_rect.right <= 0: 
+        snail_rect.left = 800 
+# 800 is the width of the screen
+    screen.blit(snail_surface, snail_rect)
+    # if you want to print something print(player_rect.left)
+    screen.blit(player_surface, player_rect)
     pg.display.update()
     clock.tick(120)
